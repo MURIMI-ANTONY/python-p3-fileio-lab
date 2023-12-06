671e88b9568455a6bf4ffa031df071b02f457732
Murimi Antony <amurimi101@gmail.com>
2023-12-06 16:37:49 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w" ) as log_file:
+        log_file.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a" ) as log_file:
+        log_file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt") as log_file:
+        return log_file.read()
